5c910188753af4721c9bf32f42a2374f0cc9424b
Daniel S. T. Hughes <dsth@cantab.net>
2010-02-04 14:47:43 +0000

    Import of DSTH/Statistics-MVA-BayesianDiscrimination-0.0.2 from CPAN.
    gitpan-cpan-distribution: Statistics-MVA-BayesianDiscrimination
    gitpan-cpan-version:      0.0.2
    gitpan-cpan-path:         DSTH/Statistics-MVA-BayesianDiscrimination-0.0.2.tar.gz
    gitpan-cpan-author:       DSTH
    gitpan-cpan-maturity:     released

--- lib/Statistics/MVA/BayesianDiscrimination.pm
+++ lib/Statistics/MVA/BayesianDiscrimination.pm
@@ -6,33 +6,37 @@ use Carp;
 use Statistics::MVA;
 use Math::Cephes qw/:explog/;
 use List::Util qw/sum/;
+use Text::SimpleTable;
 
+#=fs pod stuff
 =head1 NAME
 
 Statistics::MVA::BayesianDiscrimination - Two-Sample Linear Discrimination Analysis with Posterior Probability Calculation.
 
 =cut
+
 =head1 VERSION
 
-This document describes Statistics::MVA::BayesianDiscrimination version 0.0.1
+This document describes Statistics::MVA::BayesianDiscrimination version 0.0.2
 
 =cut
+
 =head1 DESCRIPTION
 
 Discriminant analysis is a procedure for classifying a set of observations each with k variables into predefined classes such as to allow the
 determination of the class of new observations based upon the values of the k variables for these new observations. Group membership based on linear combinations of the variables. From the set of observations where group membership is know the procedure constructs a set of linear functions, termed
 discriminant functions, such that: 
 
-    L = b_1 * x1 + b_2 * x2 +... ... + b_n * x_n - c
+    L = B[0] + B[1] * x1 + B[2] * x2 +... ... + B[n] * x_n
 
-Where c is a constant, b's are discriminant coefficients and x's are the input variables. These discriminant functions
+Where B[0] is a constant, B[n's] are discriminant coefficients and x's are the input variables. These discriminant functions
 (there is one for each group - consequently as this module only analyses data for two groups atm it generates two such
 discriminant functions.
 
-Before proceding with the analysis you should: (1) Perform Bartlett´s test to see if the covariance matrices of the data
+Before proceeding with the analysis you should: (1) Perform Bartlett´s test to see if the covariance matrices of the data
 are homogenous for the populations used (see L<Statistics::MVA::Bartlett>. If they are not homogenous you should use Quadratic Discrimination analysis.
 (2) test for equality of the group means using Hotelling's T^2 (see L<Statistics::MVA::HotellingTwoSample> or MANOVA. If
-the groups do not differ significantly it is extremely unlikely that discrimination analysis with generate any usefull
+the groups do not differ significantly it is extremely unlikely that discrimination analysis with generate any useful
 discrimination rules. (3) Specify the prior probabilities. This module allows you to do this in several ways - see
 L</priors>.
 
@@ -42,6 +46,7 @@ observation. Both of these methods are context dependent - see L</METHODS>. See
 http://en.wikipedia.org/wiki/Linear_discriminant_analysis for further details.
 
 =cut
+
 =head1 SYNOPSIS
 
     # we have two groups of data each with 3 variables and 10 observations - example data from http://www.stat.psu.edu/online/courses/stat505/data/insect.txt
@@ -95,8 +100,10 @@ http://en.wikipedia.org/wiki/Linear_discriminant_analysis for further details.
     my ($val_x, $p_x, $post_p_x, $val_y, $p_y, $post_p_y, $type) = $bld->discriminate([qw/194 124 49/]);
 
 =cut
+
 =head1 METHODS
 =cut
+
 =head2 new
 
 Creates a new Statistics::MVA::BayesianDiscrimination. This accepts two references for List-of-Lists of values
@@ -111,6 +118,7 @@ the discrimination coefficients that are accessed using the C<output> method.
     my $bld = Statistics::MVA::BayesianDiscrimination->new({priors => 1 },$data_X,$data_Y);
    
 =cut
+
 =head2 output
 
 Context-dependent method for accessing results of discrimination analysis. In void context it prints the coefficients to STDOUT.
@@ -127,6 +135,7 @@ In LIST-context it returns a list of the relevant data accessed as follows:
     print qq{\nCoefficients for discrimination function Y = @{$matrix_y}.};
 
 =cut
+
 =head2 discriminate
 
 Method for classification of a new observation. Pass it an ARRAY reference of SCALAR values appropriate for the original
@@ -143,8 +152,10 @@ In LIST-context it returns a list of the relevant data as follows:
     print qq{\nThe prior probability that the new observation is of type X = $p_y and the posterior probability = $post_p_y};     
 
 =cut
+
 =head1 OPTIONS
 =cut
+
 =head2 priors
 
 Pass within an anonymous HASH preceding the two data references during object construction:
@@ -168,21 +179,18 @@ ensure that prior_x + prior_y = 1 if you supply you own.
 
 =cut
 
-use version; our $VERSION = qv('0.0.1');
+#=fe
+
+use version; our $VERSION = qv('0.0.2');
 
 #/ with these new modules object creation directly checks data and performs analysis. storing just the essential results. can access directly or print them
 
 sub new { 
-
     my $class = shift;
-    
     #y grab an options hash if there is one
     my $options_ref = shift if ( ref $_[0] eq q{HASH} );
-   
     #y the rest is data
     my @data = @_;
-
-
     my $p_x;
     my $p_y;
 
@@ -190,7 +198,6 @@ sub new {
 
     my $k = scalar @data;
     croak qq{\nThis only accepts two groups of data} if $k != 2;
-
     #y if there´s an options hash with priors check it - otherwise default...
     if ( defined $options_ref ) { ($p_x, $p_y) = &_priors($options_ref, @data) }
     else {
@@ -198,82 +205,75 @@ sub new {
         $p_x = 0.5;
         $p_y = 0.5; 
     }
-
     #y check they have good matrix format - need to check compatibility too
     croak qq{\nbad data} if &Statistics::MVA::CVMat::_check($data[0]);
     croak qq{\nbad data} if &Statistics::MVA::CVMat::_check($data[1]);
-
     #y data is passed in table format - i.e. nested arrays (rows) correspond to observations and not variables
     my $lol1 = &Statistics::MVA::CVMat::transpose($data[0]);
     my $lol2 = &Statistics::MVA::CVMat::transpose($data[1]);
-    
     #y get variable number
     my $p = scalar @{$lol1};
     croak qq{\nThese data sets are not compatible} if ($p != scalar @{$lol2});
-
     #y combine the data for overall means calculations - if using single data with names could use adjust in MVA
     my $full = [];
     $full = &_combine($lol1,$lol2, $p);
-
     #y copy vars and we will do in place mean calculation - no need to deep copy references here
     #my $first = &_deep_copy($lol1);
     my $first = [@{$lol1}];
     my $second = [@{$lol2}];
-
     #y in place calculation of the means
     &_means($full,$p);
     &_means($first,$p);
     &_means($second,$p);
-
     #y in place subtraction of means from raw data
     &_subtract($lol1, $full);
     &_subtract($lol2, $full);
-
     #y for MVA cv_calculations the data needs to be in table format - this is wastefull as it just transpose - thus is unecessary transposes - IN PLACE - should modify this?!?
     $lol1 = &Statistics::MVA::CVMat::transpose($lol1);
     $lol2 = &Statistics::MVA::CVMat::transpose($lol2);
-
     my $mva;
     #y if we have a twat who likes this annoying method
     if (defined $options_ref && defined $options_ref->{cv} && $options_ref->{cv} == 0) { $mva = &_twats($lol1, $lol2); }
     else {
         #y create MVA object - these are defaults parameters so don´t need to pass them...
-        $mva = Statistics::MVA->new([ $lol1, $lol2   ], {standardise => 0, divisor => 1});
+        $mva = Statistics::MVA->new([ $lol1, $lol2 ], {standardise => 0, divisor => 1});
     }
-
-    #/ CV matrix data - loose last [0] for realmatrix object
+    #r CV matrix data - loose last [0] for realmatrix object
     #print Dumper $mva->[0][0][0][0];
     #print Dumper $mva->[0][1][0][0];
-
     #y create empty matrix of same dimensions
     my $c = $mva->[0][0][0]->shadow;
 
-    $mva->[0][0][0]->multiply_scalar($mva->[0][0][0],$p_x);
-    $mva->[0][1][0]->multiply_scalar($mva->[0][1][0],$p_y);
+    #/ its 2x not divided by priors?!? - only constant takes into account priors
+    #$mva->[0][0][0]->multiply_scalar($mva->[0][0][0],$p_x);
+    #$mva->[0][1][0]->multiply_scalar($mva->[0][1][0],$p_y);
+    $mva->[0][0][0]->multiply_scalar($mva->[0][0][0],0.5);
+    $mva->[0][1][0]->multiply_scalar($mva->[0][1][0],0.5);
 
     $c->add($mva->[0][0][0],$mva->[0][1][0]); 
 
     #y no need for transpose just build from rows...
     my $averages_a = Math::MatrixReal->new_from_rows([$first]);
     my $averages_b = Math::MatrixReal->new_from_rows([$second]);
-
     #y calculate b_0 - constant discrimination coefficients
     my $constant_x = 0.5 * $averages_a * $c->inverse * ~$averages_a;
     my $constant_y = 0.5 * $averages_b * $c->inverse * ~$averages_b;
 
+    #/ you must add the priors to the constant - only the constant takes into account the priors.
+    #$constant_x->[0][0][0] = $constant_x->[0][0][0] - log($p_x);
+    #$constant_y->[0][0][0] = $constant_y->[0][0][0] - log($p_y);
+    #y now should be -0.5...
+    $constant_x->[0][0][0] = -$constant_x->[0][0][0] + log($p_x);
+    $constant_y->[0][0][0] = -$constant_y->[0][0][0] + log($p_y);
+
     #y calculate the rest of the discrimination coeficients
     #~$averages_a * $c->inverse * $averages_a;
-    #print $c->inverse * ~$averages_a;
-    #my $temp =  $c->inverse * ~$averages_a;
-    #print qq{\nhi}, $averages_a * $temp;
     my $matrix_x = $averages_a * $c->inverse;
     my $matrix_y = $averages_b * $c->inverse;
 
     #y feed the objectvv- this is only done for persistance - otherwise we loose the data...
-    #$self->[0][0][0] = $constant_x->[0][0][0];
     $self->[0][0][0] = $constant_x;
     $self->[0][0][1] = $matrix_x;
-    #$self->[0][1][0] = $constant_y->[0][0][0];
     $self->[0][1][0] = $constant_y;
     $self->[0][1][1] = $matrix_y;
     $self->[1][0] = $p_x;
@@ -281,7 +281,6 @@ sub new {
     $self->[2] = $p;
 
     bless $self, $class;
-
     return $self;
 }
 
@@ -289,7 +288,6 @@ sub _priors {
     my ($options_ref, @data) = @_;
     my $p_x;
     my $p_y;
-    
     if ($options_ref->{priors} ==  1 ) {
         my $n_x = scalar @{$data[0]};
         my $n_y = scalar @{$data[1]};
@@ -300,7 +298,7 @@ sub _priors {
     elsif (ref $options_ref->{priors} eq q{ARRAY} ) {
         my @priors = @{$options_ref->{priors}};
         croak qq{\nIf passing an ARRAY ref of priors it must have length two.} if ( scalar @priors != 2 );
-        #for my $i (@priors) { croak qq{\nPriors must be numeric.} if $i !~ /\A[+?\*(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?\z/xms; }
+        croak qq{\nThe priors must sum to 1.} if ( $priors[0] + $priors[1] < 0.95 || $priors[0] + $priors[1] > 1.05 );
         $p_x = $priors[0];
         $p_y = $priors[1];
     }
@@ -324,7 +322,6 @@ sub _subtract {
 sub _combine{
     my ($lol1, $lol2, $p) = @_;
     my $full = [];
-    #for my $i (0..$#{$ĺol1}) {
     for my $i (0..$p-1) {
         push @{$full->[$i]}, @{$lol1->[$i]}, @{$lol2->[$i]};
     }
@@ -341,9 +338,7 @@ sub _means {
 }
 
 sub _twats {
-
     my ($lol1, $lol2) = @_;
-
     my $lol1_mat = Math::MatrixReal->new_from_rows($lol1);
     my $n_1 = scalar @{$lol1};
     $lol1_mat = (~$lol1_mat * $lol1_mat) / $n_1;
@@ -355,14 +350,11 @@ sub _twats {
     $mva->[0][1][0] = $lol2_mat;
     print $mva->[0][0][0];
     print $mva->[0][1][0];
-
     return $mva;
 }
 
 sub output {
-
     my $self = shift;
-
     my $constant_x = $self->[0][0][0];
     my $constant_y = $self->[0][1][0];
     my $matrix_x = $self->[0][0][1];
@@ -370,39 +362,26 @@ sub output {
     my $p_x = $self->[1][0]; 
     my $p_y = $self->[1][1]; 
     my $p_var_num = $self->[2]; 
-
-    #$self->[0][1][0] = $constant_y->[0][0][0];
-    #$self->[0][1][0] = $constant_y;
-    #$#self->[0][1][1] = $matrix_y;
-
     if (!wantarray) {
-        print qq{\nTwo-groups with $p_var_num variables each and prior probabilities of $p_x and $p_y.};
-        print qq{\n\nDiscriminant function for the first group has constant B[0] = $constant_x->[0][0][0] and coefficient};
-        for (0..$#{$matrix_x->[0][0]}) { 
-            my $punct = $_ == $#{$matrix_x->[0][0]} ? q{.} : q{, };  
-            my $blah = $_+ 1;
-            print qq{B[$blah] = $matrix_x->[0][0][$_]$punct}; 
+        print qq{\nTwo-groups with $p_var_num variables each and prior probabilities of p(x) = $p_x and p(y) = $p_y.\n\n};
+        my @config = ( [17, q{}], [15, q{X}], [15, q{Y}] );
+        my $tbl = Text::SimpleTable->new(@config);
+        $tbl->row( qq{Constant B[0]}, sprintf(q{%.5f}, $constant_x->[0][0][0]), sprintf(q{%.5f}, $constant_y->[0][0][0]) );
+        $tbl->hr;
+        for my $row (0..$#{$matrix_x->[0][0]}) { 
+            my $coeff = $row+1;
+            $tbl->row( qq{Coefficient B[$coeff]}, sprintf(q{%.5f}, $matrix_x->[0][0][$row]), sprintf(q{%.5f}, $matrix_y->[0][0][$row]) );
+            $tbl->hr if $row != $#{$matrix_x->[0][0]};
         }
-        print qq{\nDiscriminant function for the first group has constant B[0] = $constant_y->[0][0][0] and coefficient};
-        for (0..$#{$matrix_y->[0][0]}) { 
-            my $punct = $_ == $#{$matrix_y->[0][0]} ? q{.} : q{, };  
-            my $blah = $_+ 1;
-            print qq{B[$blah] = $matrix_y->[0][0][$_]$punct}; 
-        }
-        print qq{\n};
+        print $tbl->draw;
         return;
     }
-    #else { return [ [ $p_x, $constant_x->[0][0][0], $matrix_x->[0][0] ], [ $p_y, $constant_y->[0][0][0], $matrix_y->[0][0] ] ]; }
     else { return ( $p_x, $constant_x->[0][0][0], $matrix_x->[0][0], $p_y, $constant_y->[0][0][0], $matrix_y->[0][0] ); }
 }
 
 sub discriminate {
-
     my ($self, $ex) = @_;
-
-    #y/ make it context dependent between list and anything else - i.e. LIST gives a list anything else it prints and returns nothing
     my $context = wantarray;
-
     my $constant_x = $self->[0][0][0];
     my $constant_y = $self->[0][1][0];
     my $matrix_x = $self->[0][0][1];
@@ -415,19 +394,17 @@ sub discriminate {
     croak qq{\nThis example has the wrong variable number} if scalar @{$ex} != $p_var_num;
 
     my $example = Math::MatrixReal->new_from_cols([$ex]);
-
-    #my $val = ( $averages_a * $c->inverse * $example ) ;
-    my $val_x = ( $matrix_x * $example ) - $constant_x;#; + log($p_x);
+    my $val_x = ( $matrix_x * $example ) + $constant_x;#; + log($p_x);
 
     #r when adding log(p) its the linear score function and not simply the linear discrimination result
-    $val_x = $val_x->[0][0][0]+log($p_x);
-    #$val_x = $val_x->[0][0][0];
-    my $val_y = ( $matrix_y * $example ) - $constant_y;#; + log($p_x);
-    $val_y = $val_y->[0][0][0]+log($p_y);
-    #$val_y = $val_y->[0][0][0];
+    #$val_x = $val_x->[0][0][0];#+log($p_x);
+    $val_x = $val_x->[0][0][0];
+    my $val_y = ( $matrix_y * $example ) + $constant_y;#; + log($p_x);
+    #$val_y = $val_y->[0][0][0];#+log($p_y);
+    $val_y = $val_y->[0][0][0];
 
-    my $type    = $val_x > $val_y ? q{x}
-                : $val_y > $val_x ? q{y} 
+    my $type    = $val_x > $val_y ? q{X}
+                : $val_y > $val_x ? q{Y} 
                 :                   undef;
 
     my $post_p_x = exp($val_x) / (exp($val_x) + exp($val_y));
@@ -435,19 +412,15 @@ sub discriminate {
 
     if (!$context) {
         print qq{\nLinear score function for x = $val_x\nLinear score function for y = $val_y\n};
-
-        if ( defined $type  ) { print qq{\nExample is a $type.} } 
+        if ( defined $type  ) { print qq{\nExample (@{$ex}) is a \x27$type\x27.} } 
         else { print qq{\nCannot distinguish which group the example belongs to.} }
-
         print qq{\n\nPrior probability of being an x = $p_x\nPosterior probability of being an x = $post_p_x\n};
         print qq{\nPrior probability of being a y = $p_y\nPosterior probability of being a y = $post_p_y\n};
         return;
     }
     else {
-
         return ($val_x, $p_x, $post_p_x, $val_y, $p_y, $post_p_y, $type); 
     }
-
 }
 
 1; # Magic true value required at end of module
@@ -469,18 +442,22 @@ __END__
 'Carp' => '1.08', 
 'Math::Cephes' => '0.47', 
 'List::Util' => '1.19',
+'Text::SimpleTable' => '2.0',
 
 =cut
+
 =head1 BUGS
 
 Let me know.
 
 =cut
+
 =head1 AUTHOR
 
 Daniel S. T. Hughes  C<< <dsth@cantab.net> >>
 
 =cut
+
 =head1 LICENCE AND COPYRIGHT
 
 Copyright (c) 2010, Daniel S. T. Hughes C<< <dsth@cantab.net> >>. All rights reserved.
@@ -489,6 +466,7 @@ This module is free software; you can redistribute it and/or
 modify it under the same terms as Perl itself. See L<perlartistic>.
 
 =cut
+
 =head1 DISCLAIMER OF WARRANTY
 
 Because this software is licensed free of charge, there is no warranty
